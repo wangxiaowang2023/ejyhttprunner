226963cb55fbd9a6b0ecd86b679815b6126fc889
deanywangmou <243688409@qq.com>
2022-12-12 18:16:47 +0800

    pre环境修改字段

--- runtest.py
+++ runtest.py
@@ -2,11 +2,9 @@ import os
 from yjyhrp.util.runner import yjytest
 
 if __name__ == '__main__':
-    set_env = 'DEV'
+    set_env = 'PRE'
     os.environ['TEST_ENV'] = set_env
     runtest = yjytest(set_env, 'info')
-    runtest.runtestcase(test_path='testsuites/P端接口/API对接主要接口流程/通用api主流程测试集.yml', report_title='小前端P端api测试集报告')
-    # runtest.runtestcase(test_path='testcases/P端接口/h5模块登录', report_title='小前端测试报告')
-    # runtest.runtestcase(test_path='testcases/P端接口/API对接主要接口/用户兑换优惠券.yml', report_title='小前端测试报告')
-    # runtest.runtestcase(test_path='testcases/P端接口/API对接主要接口', report_title='小前端测试报告')
-
+    # runtest.runtestcase(test_path='testsuites/P端接口/API对接主要接口流程/dev环境通用api主流程测试集.yml', report_title='小前端dev环境P端api测试集报告')
+    runtest.runtestcase(test_path='testsuites/P端接口/API对接主要接口流程/pre环境通用api主流程测试集.yml',
+                        report_title='小前端pre环境P端api测试集报告')
