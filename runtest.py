1ba89243bfff54ddfe6fa7ab71d078b39df107e5
deanywangmou <243688409@qq.com>
2022-12-22 14:54:57 +0800

    修改pre平台字段

--- runtest.py
+++ runtest.py
@@ -2,7 +2,7 @@ import os
 from yjyhrp.util.runner import yjytest
 
 if __name__ == '__main__':
-    set_env = 'DEV'
+    set_env = 'PRE'
     os.environ['TEST_ENV'] = set_env
     runtest = yjytest(set_env, 'info')
     if set_env=='DEV':
