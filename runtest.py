650cca49b94a2930fcc49e84b0c26ef1b208ce37
deanywangmou <243688409@qq.com>
2023-01-02 18:31:40 +0800

    修改编码格式

--- runtest.py
+++ runtest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding:utf-8
 import os
 from yjyhrp.util.runner import yjytest
 
@@ -5,7 +7,7 @@ if __name__ == '__main__':
     set_env = 'PRE'
     os.environ['TEST_ENV'] = set_env
     runtest = yjytest(set_env, 'info')
-    if set_env=='DEV':
+    if set_env == 'DEV':
         runtest.runtestcase(test_path='testsuites/P端接口/API对接主要接口流程/dev环境通用api主流程测试集.yml',
                             report_title='小前端dev环境P端api测试集报告')
     else:
